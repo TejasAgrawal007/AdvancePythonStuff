5ace018db443b765deecbfb42b81c2e4639214c9
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-02-21 22:05:47 +0530

    Update 02_ControlStructure.py

--- 02_ControlStructure.py
+++ 02_ControlStructure.py
@@ -1,4 +1,4 @@
-# 1. Marks
+# 1. Makrs
 # def moderate(marks, passMakrs):
 #     if marks == passMakrs - 1 or marks == passMakrs -2 :
 #         marks = passMakrs
@@ -12,7 +12,6 @@
 
 # if __name__ == "__main__":
 #     main()
-
 
 
 # 2. authenticateUser System Program
@@ -36,7 +35,6 @@
 #     main()
 
 
-
 # assignGrade Program.
 
 # Note  --> Assert statement used to debug code and handle  the Condition Code.
@@ -45,13 +43,13 @@
 #     assert marks >= 0 and marks <= 100
 #     if marks >= 90:
 #         grade = "A"
-    
+
 #     elif marks >= 70:
 #         grade =  "B"
-    
+
 #     elif marks >= 50:
 #         grade =  "C"
-    
+
 #     elif marks >= 40:
 #         grade =  "D"
 #     else:
@@ -97,7 +95,6 @@
 #     main()
 
 
-
 # Program To Find Some Of First N Positive Number.
 
 # def summation(n):
@@ -108,7 +105,7 @@
 
 
 # def main():
-#     n = int(input("Enter The Number of Terms: "))
+#     n = int(input("Enter The Number of Turms: "))
 #     total = summation(n)
 #     print("Sum of First", n , "Positive Integer: ", total)
 
@@ -117,10 +114,7 @@
 #     main()
 
 
-
-
-
-# Program to Compute  Percentage
+# Program to Compute a Percentage
 
 # def main():
 #     n = int(input("Enter The Total Subject: "))
@@ -136,3 +130,133 @@
 
 # if __name__ == "__main__":
 #     main()
+
+
+# Loop for printing a Multiplactin table.
+
+
+# from numpy import product
+
+
+# num = int(input("Enter The Number: "))
+# for i in range(1, 11):
+#     product = num * i
+#     print(f"The Number is {i} X {num} = {i*num}")
+
+# def printTable(num, i = 10):
+#         for i in range(1, 11):
+#             product = num * i
+#             print(f"The Number is {i} X {num} = {i*num}")
+
+
+# def main():
+#     num = int(input("Enter The Number: "))
+#     printTable(num)
+
+# if __name__ == "__main__":
+#     main()
+
+
+# program to Compute Sum of Numbers
+
+
+# from numpy import number
+
+
+# def main():
+#     total  = 0
+#     number = input("Enter a Number: ")
+#     while number != "":
+#         total += int(number)
+#         number = input("Enter a Number: ")
+#     print("Sum of the Number is = ", total)
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+# Program to Chech the Number is Prime or Not
+
+# from unittest import result
+
+
+# def prime(n):
+#     if n == 1:
+#         return False
+
+#     for i in range(2, n):
+#         if(n%i == 0):
+#             return False
+#         else:
+#             return True
+
+
+# def main():
+#     n = int(input("Enter The Number: "))
+#     result = prime(n)
+#     if result ==  True:
+#         print("the Number is Prime")
+#     else:
+#         print("The Number is not Prime")
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+# Write a Program To Print The RightAngleTriangele
+
+# def rightAngleTriangele(n):
+#     for i in range(1, n+1):
+#         print(" * " * i)
+
+# def main():
+#     n = int(input("Enter The Number: "))
+#     rightAngleTriangele(n)
+
+# if __name__ == "__main__":
+#     main()
+
+# Inverted triangle
+
+# def InvertedTriangle(n):
+#     spaces = 0
+#     star = 2 * n - 1
+#     for i in range(1, n + 1):
+#         print(" " * spaces, "*" * star)
+#         spaces += 1
+#         star -= 2
+
+# def main():
+#     n = int(input("Enter The Number of Rows: "))
+#     InvertedTriangle(n)
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+# Compute Selling price
+
+# def discount(price):
+#     pass
+
+
+# def selling(price):
+#     dicountPrice = discount(price)
+#     if dicountPrice == None:
+#         return price
+#     else:
+#         return dicountPrice
+
+# def main():
+#     price = float(input("Enter The Number: "))
+#     print("The Selling price is ", selling(price))
+    
+
+
+# if __name__ == "__main__":
+#     main()
+
